f15db52afc3bc4c42b94090cad408f5f6aedf0dc
Evan Phoenix <ephoenix@engineyard.com>
2009-06-06 18:52:31 -0700

    Report the branch name

--- lib/irc_cat/http_server/github.rb
+++ lib/irc_cat/http_server/github.rb
@@ -3,7 +3,10 @@ require 'json/add/rails'
 
 class Github < Mongrel::HttpHandler
   
-  def initialize(bot, config); @bot = bot; @config = config; puts "Github enabled"; end
+  def initialize(bot, config)
+    @bot = bot; @config = config
+    puts "Github enabled"
+  end
   
   def process(request, response)
     response.start(200) do |head,out|
@@ -11,11 +14,16 @@ class Github < Mongrel::HttpHandler
       post = CGI.parse(request.body.read)
       json = JSON.parse(post['payload'].join(','))
       channel = @config['irc']['channel']
+      branch = json['ref'].split("/").last
       json['commits'].each do |c|
         topic = c['message'].split("\n").first
         ref =   c['id'][0,7]
         author = c['author']['name']
-        @bot.say channel, "#{topic} - #{ref} - #{author}"
+        if branch != "master"
+          @bot.say channel, "#{topic} - #{ref} - #{author} (#{branch})"
+        else
+          @bot.say channel, "#{topic} - #{ref} - #{author}"
+        end
       end
     end
   end
